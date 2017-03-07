b88d443cec1d6d22c6f0eeeb657e050f2d303a5f
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2017-03-07 20:19:42 +0530

    voting invokes callback

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //noinspection JSUnresolvedVariable
-import { renderIntoDocument, scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -18,4 +18,17 @@ describe('Voting', () => {
 		expect(buttons[1].textContent).to.equal('28 Days Later');
 	});
 
+	it('invokes callback when a button is clicked', () => {
+		let votedWith = '';
+		const vote = (entry) => votedWith = entry;
+
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
+		);
+
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		Simulate.click(buttons[0]);
+		expect(votedWith).to.equal('Trainspotting');
+	});
 });
