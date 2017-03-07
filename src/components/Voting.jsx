071e4caf0cd107ca781435f7c9a9da7b8173355b
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2017-03-07 20:07:35 +0530

    voting test

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,16 +4,13 @@ export default React.createClass({
 	getPair: function() {
 		return this.props.pair || [];
 	},
-
 	render: function() {
-		return (
-			<div className="voting">
-				{this.getPair().map(entry => 
-					<button  key={entry}>
-						<h1>{entry}</h1>
-					</button>
-				)}
-			</div>
-		);
+		return <div className="voting">
+			{this.getPair().map(entry =>
+				<button key={entry}>
+					<h1>{entry}</h1>
+				</button>
+			)}
+		</div>;
 	}
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -4,14 +4,24 @@ import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
+// get the window object out of the document
 
+// set globals for mocha that make access to document and window feel
+// natural in the test environment
 global.document = doc;
 global.window = win;
 
-Object.keys(window).forEach((key) => {
-	if(!key in global) {
-		global[key] = window[key];
-	}
-});
+// take all properties of the window object and also attach it to the
+// mocha global object
+propagateToGlobal(win);
 
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+	for (let key in window) {
+		if (!window.hasOwnProperty(key)) continue
+		if (key in global) continue
+
+		global[key] = window[key]
+	}
+}
 chai.use(chaiImmutable);
